11f99c0c1936755cfd0a664b9696f3fda1c0ea8c
Avery Kyle Arenas <kyruken@gmail.com>
2023-04-25 15:27:07 -0700

    add conditional rendering for each game phase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [gameEnded, setGameEnded] = useState(false);
+
+  const startGame = () => {
+    setIsPlaying(true);
+  }
+
+  const endGame = () => {
+    setIsPlaying(false);
+    setGameEnded(true);
+  }
+
   return (
     <div className="App">
-      <h1>App</h1>
+      <h1>Hangman</h1>
+
+      {!isPlaying && !gameEnded && 
+        <div>
+          <h2>Click button to play hangman</h2>
+          <button onClick={startGame}>Start Game</button>
+        </div>}
+      
+      {isPlaying &&
+        <div>
+          <h2>(Game of hangman)</h2>
+          <button onClick={endGame}>game over button</button>
+        </div>}
+
+      {gameEnded && !isPlaying &&
+        <div>
+          <h2>Game over</h2>
+        </div>}
     </div>
   );
 }
