1e73e031c6aeb5149d35e81b873c21f5c1203eaf
Phuong Tran <fuongdtran@gmail.com>
2023-04-26 03:13:58 -0700

    can play the game given hardcoded phrase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import { useState } from "react";
+
+import { Box, TextField, Button } from '@mui/material';
+
 import './App.css';
 
 function App() {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [wordPhrase, setWordPhrase] = useState('');
+  const [correctGuess, setCorrectGuess] = useState('');
+  const [wrongGuess, setWrongGuess] = useState('');
+
+  const [currentGuess, setcurrentGuess] = useState('');
 
   const startGame = () => {
     setIsPlaying(true);
+    // TODO: randomize a phrase, all upper case
+    setWordPhrase("HELLO WORLD");
   }
 
   const endGame = () => {
     setIsPlaying(false);
+  }
+
+  const handleGuessInput = e => {
+    const upperCaseGuess = currentGuess.toUpperCase();
+    if (wordPhrase.includes(upperCaseGuess)) {
+      setCorrectGuess(correctGuess + upperCaseGuess);
+    } else {
+      setWrongGuess(wrongGuess + upperCaseGuess);
+    }
+    setcurrentGuess('');
   }
 
   return (
@@ -17,15 +37,50 @@ function App() {
       <h1>Hangman</h1>
 
       {!isPlaying ? <div>
-          <h2>Click button to play hangman</h2>
-          <button onClick={startGame}>Start Game</button>
-        </div>
+        <h2>Click button to play hangman</h2>
+        <button onClick={startGame}>Start Game</button>
+      </div>
         :
         <div>
           <h2>(Game of hangman)</h2>
           <button onClick={endGame}>game over button</button>
         </div>}
 
+      {isPlaying && (
+        <Box sx={{ width: '200px', display: 'flex', flexDirection: 'column' }}>
+          <label> {`Incorrect guesses: ${wrongGuess.split('').toString()}`}</label>
+          <TextField id="outlined-basic" label="Your Guess" variant="outlined" value={currentGuess} onChange={e => setcurrentGuess(e.target.value)} inputProps={{ maxLength: 1 }} />
+          <Button variant="outlined" onClick={handleGuessInput}>Make a guess</Button>
+        </Box>
+      )}
+
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        {
+          wordPhrase && wordPhrase.split("").map((char, idx) => {
+            if (char === ' ') {
+              return (
+                <Box key={idx} sx={{
+                  width: 40,
+                  height: 40,
+                }} />)
+            } else {
+              return (
+                <Box
+                  key={idx}
+                  sx={{
+                    width: 40,
+                    height: 40,
+                    backgroundColor: 'primary.dark',
+                    m: 0.2,
+                    textAlign: 'center',
+                    justify: 'center'
+                  }}>
+                  <label>{correctGuess.includes(char) ? char : ''}</label>
+                </Box>)
+            }
+          })
+        }
+      </div>
     </div>
   );
 }
